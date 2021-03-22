3839f12a445ace96fdafa5ae1264f13f4582d57c
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-03-22 18:46:10 -0500

    read, write and delete files using FS

--- modulos/fileSistem.js
+++ modulos/fileSistem.js
@@ -1,1 +1,32 @@
 const fs = require("fs");
+
+let leer = (ruta, callbackLeer) => {
+  fs.readFile(ruta, (error, data) => {
+    // leyendo
+    callbackLeer(data.toString());
+  });
+};
+
+// leer(__dirname + "/archivo1.txt", console.log);
+
+let escribir = (ruta, contenido, callbalEscribir) => {
+  fs.writeFile(ruta, contenido, (error) => {
+    if (error) {
+      console.error("no se pudo escribir el archivo");
+    } else {
+      console.log("escrito correctamente");
+    }
+  });
+};
+
+// escribir(
+//   __dirname + `/archivo1.txt`,
+//   " soy el nuevo archivo creado con FS",
+//   console.log
+// );
+
+let borrar = (ruta, callbackBorrar) => {
+  fs.unlink(ruta, callbackBorrar);
+};
+
+borrar(__dirname + "/archivo1.txt", console.log);
