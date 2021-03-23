68e4f912f6654efe7dbcf0719c69d73e45dd2a09
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-03-23 12:19:53 -0500

    ejecutar modulos de C++ en NodeJS

---
+++ modulos/nativos/build/Release/.deps/Release/obj.target/addon/hola.o.d
@@ -0,0 +1,21 @@
+cmd_Release/obj.target/addon/hola.o := g++ -o Release/obj.target/addon/hola.o ../hola.cc '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/gokyan/.cache/node-gyp/14.15.4/include/node -I/home/gokyan/.cache/node-gyp/14.15.4/src -I/home/gokyan/.cache/node-gyp/14.15.4/deps/openssl/config -I/home/gokyan/.cache/node-gyp/14.15.4/deps/openssl/openssl/include -I/home/gokyan/.cache/node-gyp/14.15.4/deps/uv/include -I/home/gokyan/.cache/node-gyp/14.15.4/deps/zlib -I/home/gokyan/.cache/node-gyp/14.15.4/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/hola.o.d.raw   -c
+Release/obj.target/addon/hola.o: ../hola.cc \
+ /home/gokyan/.cache/node-gyp/14.15.4/include/node/node.h \
+ /home/gokyan/.cache/node-gyp/14.15.4/include/node/v8.h \
+ /home/gokyan/.cache/node-gyp/14.15.4/include/node/cppgc/common.h \
+ /home/gokyan/.cache/node-gyp/14.15.4/include/node/v8config.h \
+ /home/gokyan/.cache/node-gyp/14.15.4/include/node/v8-internal.h \
+ /home/gokyan/.cache/node-gyp/14.15.4/include/node/v8-version.h \
+ /home/gokyan/.cache/node-gyp/14.15.4/include/node/v8config.h \
+ /home/gokyan/.cache/node-gyp/14.15.4/include/node/v8-platform.h \
+ /home/gokyan/.cache/node-gyp/14.15.4/include/node/node_version.h
+../hola.cc:
+/home/gokyan/.cache/node-gyp/14.15.4/include/node/node.h:
+/home/gokyan/.cache/node-gyp/14.15.4/include/node/v8.h:
+/home/gokyan/.cache/node-gyp/14.15.4/include/node/cppgc/common.h:
+/home/gokyan/.cache/node-gyp/14.15.4/include/node/v8config.h:
+/home/gokyan/.cache/node-gyp/14.15.4/include/node/v8-internal.h:
+/home/gokyan/.cache/node-gyp/14.15.4/include/node/v8-version.h:
+/home/gokyan/.cache/node-gyp/14.15.4/include/node/v8config.h:
+/home/gokyan/.cache/node-gyp/14.15.4/include/node/v8-platform.h:
+/home/gokyan/.cache/node-gyp/14.15.4/include/node/node_version.h:
